6c3e9519f90def3e6e4cf8e24966c79f9751045e
greybo <sergeybotl@gmail.com>
2023-02-18 19:56:16 +0200

    add message component

---
+++ app/src/main/java/com/example/composeapp/ui/component/tutorial/TutorialComponent.kt
@@ -0,0 +1,42 @@
+package com.example.composeapp.ui.component.tutorial
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.itemsIndexed
+import androidx.compose.material3.Divider
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.composeapp.ui.component.tutorial.example1.MessageCard
+import com.example.composeapp.ui.component.tutorial.example1.MessageViewModel
+
+@Composable
+fun TutorialGreeting(viewModel: MessageViewModel = viewModel()) {
+
+    val itemsList by viewModel.uiState.collectAsState()
+
+    LazyColumn(
+        verticalArrangement = Arrangement.spacedBy(12.dp),
+    ) {
+        itemsIndexed(itemsList) { index, item ->
+            MessageCard(item, index) { itemAdapter, indexAdapter ->
+                viewModel.changeData(itemAdapter, indexAdapter)
+            }
+//            Text("Item at index $index is $item")
+
+            if (index < itemsList.lastIndex)
+                Divider(color = Color.Red, thickness = 1.dp)
+        }
+    }
+
+}
+
+@Preview()
+@Composable
+fun PreviewTutorialGreeting() {
+    TutorialGreeting()
+}
---
+++ app/src/main/java/com/example/composeapp/ui/component/tutorial/example1/MessageViewModel.kt
@@ -0,0 +1,39 @@
+package com.example.composeapp.ui.component.tutorial.example1
+
+import androidx.lifecycle.ViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.getAndUpdate
+
+data class Message(val author: String, val body: String)
+
+class MessageViewModel : ViewModel() {
+
+    val itemsList = arrayListOf<Message>(
+        Message("author", "Hey, take a look at Jetpack Compose, it's great! Hey, take a look at Jetpack Compose, it's great!"),
+        Message("author", "Congratulations, you’ve finished the Compose tutorial! You’ve built a simple chat screen efficiently showing a list of expandable & animated messages containing an image and texts, designed using Material Design principles with a dark theme included and previews—all in under 100 lines of code!"),
+        Message("author", "body"),
+        Message("author", "body"),
+        Message("author", "body"),
+        Message("author", "body"),
+        Message("author", "body"),
+    )
+
+
+    private val _uiState = MutableStateFlow(itemsList)
+    val uiState: StateFlow<List<Message>> = _uiState.asStateFlow()
+
+    fun changeData(model: Message, index: Int) {
+
+        val changed = if (model.author == "author") model.copy(author = "Colleague")
+        else if (model.body == "body") model.copy(body = "Hey, take a look at Jetpack Compose, it's great!")
+        else model.copy(author = "author", body = "body")
+        _uiState.getAndUpdate {
+            it[index] = changed
+            it
+        }
+    }
+}
+
+fun getFakeMessage() = Message("Colleague", "Hey, take a look at Jetpack Compose, it's great!")
