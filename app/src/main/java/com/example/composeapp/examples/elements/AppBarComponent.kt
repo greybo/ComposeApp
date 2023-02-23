36c2f8a356297fc96bf42ff09825799c4c406f0c
greybo <sergeybotl@gmail.com>
2023-02-23 17:53:09 +0200

    fix packages

--- app/src/main/java/com/example/composeapp/examples/elements/AppBarComponent.kt
+++ app/src/main/java/com/example/composeapp/examples/elements/AppBarComponent.kt
@@ -1,4 +1,4 @@
-package com.example.composeapp.examples
+package com.example.composeapp.examples.elements
 
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
--- app/src/main/java/com/example/composeapp/examples/elements/FabComponent.kt
+++ app/src/main/java/com/example/composeapp/examples/elements/FabComponent.kt
@@ -1,4 +1,4 @@
-package com.example.composeapp.examples
+package com.example.composeapp.examples.elements
 
 //import androidx.compose.material3.FloatingActionButton
 //import androidx.compose.material3.FloatingActionButtonDefaults
