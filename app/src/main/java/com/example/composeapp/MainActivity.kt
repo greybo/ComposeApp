0e392f9d12278844d7678dd314079b4286df8f5e
greybo <sergeybotl@gmail.com>
2023-02-24 19:37:26 +0200

    SwipeToDismissComponent

--- app/src/main/java/com/example/composeapp/MainActivity.kt
+++ app/src/main/java/com/example/composeapp/MainActivity.kt
@@ -5,12 +5,13 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.ExperimentalMaterialApi
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.example.composeapp.examples.RailComp
+import com.example.composeapp.examples.SwipeToDismissComponent
 import com.example.composeapp.ui.theme.ComposeAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -30,6 +31,7 @@ class MainActivity : ComponentActivity() {
     }
 }
 
+@OptIn(ExperimentalMaterialApi::class)
 @Composable
 fun Greeting() {
     Column {
@@ -48,8 +50,9 @@ fun Greeting() {
 //        BackdropScaffoldComponent()
 //        AlertDialogComponent()
 //        AlertDialogComponentMain()
-        RailComp()
+//        RailComp()
 //        AnimationComponent()
+        SwipeToDismissComponent()
     }
 }
 
